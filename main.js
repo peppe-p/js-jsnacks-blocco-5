bb2b3be1a93c0eea1920da6f01f079c825088703
peppe-p <ombrachiara96@gmail.com>
2021-05-11 11:28:45 +0200

    Complete JSnack 4-5

--- main.js
+++ main.js
@@ -63,8 +63,8 @@ console.log(peso_totale);
 
 
 //////////////////////////////////////////////////////////////////////////////////////
-/* Snack 2:
-Crea 10 oggetti che rappresentano una zucchina.*/
+//Snack 2:
+//Crea 10 oggetti che rappresentano una zucchina.
 //Dividi in due array separati le zucchine che misurano meno o più di 15cm.
 var zucchine_xs = [];
 var zucchine_xl = [];
@@ -82,8 +82,8 @@ console.log(zucchine_xl);
 console.log(zucchine_xs);
 
 //////////////////////////////////////////////////////////////////////////////////////
-/* Snack 3:
-Scrivi una funzione che accetti una stringa come argomento e la ritorni girata(es.Ciao -> oaiC) */
+//Snack 3:
+//Scrivi una funzione che accetti una stringa come argomento e la ritorni girata(es.Ciao -> oaiC)
 function reverseWord(txt) {
     var result = [];
     for (var i = 0; i < txt.length; i++) {
@@ -94,12 +94,39 @@ function reverseWord(txt) {
 var word = prompt("Inserisci una parola");
 console.log(reverseWord(word));
 
+
+//////////////////////////////////////////////////////////////////////////////////////
 /* Snack 4:
 Scrivi una funzione che fonda due array(aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro
 es. [a, b, c], [1, 2, 3]→[a, 1, b, 2, c, 3]. */
+var numeri = ["1", "2", "3", "4", "5"];
+var lettere = ["a", "b", "c", "d", "e"]
+var newArray = [];
+function arrayFusion(arr1, arr2) {
+    for (var i = 0; i < arr1.length; i++) {
+        console.log(arr1[i]);
+        console.log(arr2[i]);
+        newArray.push(arr1[i], arr2[i]);
+    }
+}
+arrayFusion(numeri, lettere);
 
 
+//////////////////////////////////////////////////////////////////////////////////////s
 /* Snack 5:
 Scrivi una funzione che accetti tre argomenti:
 un array e due numeri(“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b” */
+function filterArray(list, min, max) {
+    var newArray = list;
+    newArray.splice(max, 1);
+    newArray.splice(min, 1);
+    return newArray;
+}
+
+var colori = ["Rosso", "Blu", "Viola", "Verde", "Giallo"];
+var n1 = 0;
+var n2 = 4;
+
+filterArray(colori, n1, n2);
+console.log(colori);
